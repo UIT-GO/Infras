d6db23627d3b6bc926ec94232ebd2f0b4e2e32df
mainguyenuit <22520991@gm.uit.edu.vn>
2025-11-29 16:27:05 +0700

    update

--- scripts/init-mongodb-cluster.sh
+++ scripts/init-mongodb-cluster.sh
@@ -13,8 +13,8 @@ docker exec -it mongodb mongosh --eval "
 rs.initiate({
   _id: 'rs0',
   members: [
-    { _id: 0, host: 'PRIVATE_IP_1:27017' },
-    { _id: 1, host: 'PRIVATE_IP_2:27017' }
+    { _id: 0, host: '10.10.1.156:27017' },
+    { _id: 1, host: '10.10.2.162:27017' }
   ]
 })
 "
